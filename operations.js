0d0d99035cc7d4d7078676501294fa9e5b495167
GRaviteja123 <gunda.teja4@gmail.com>
2024-01-19 11:39:41 +0530

    commit

---
+++ operations.js
@@ -0,0 +1,25 @@
+import mysql from 'mysql2';
+
+
+
+
+const connection = mysql.createPool({
+    host:'127.0.0.1',
+    user : 'root',
+    password: 'Trilok@225',
+    database:'booking_app'
+}).promise()
+
+export async function readUser(username) {
+    const output = await connection.query("select * from users where username=?",[username])
+    return output[0];
+    
+}
+
+export async function insertUser(username,name,password){
+    const result = await connection.query('INSERT INTO users (username,name,password) VALUES (?,?,?)', [username,name,password]);
+}
+export async function checkUsername(username){
+    const result = await connection.query('SELECT * FROM users WHERE username = ?', [username]);
+    return(result[0].length)
+}
